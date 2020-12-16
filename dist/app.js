abd7dae8705e3ca46d6f3b50f04d237e70ad4928
matheus <matheuzv777@gmail.com>
2020-12-16 20:34:56 -0300

    adicionado express e o ts-node

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+console.log('server rodando');
+var app = express_1.default();
+app.listen(3333, function () {
+    console.log('server rodando');
+});
