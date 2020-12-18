a79a63f0eb417e65d9279499b30bd4602f915111
matheus778 <matheuzv777@gmail.com>
2020-12-17 22:25:54 -0300

    adicionado cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.use((req, res, next)=>{
+  res.header("Access-Control-Allow-Origin", "*");
+  app.use(cors())
+  next();
+});
 
 app.use(routes);
 
